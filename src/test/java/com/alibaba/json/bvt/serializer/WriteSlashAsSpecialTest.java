6bc216d32aa4d459ce4b7b0a0551aed691bb77ed
Lauris BH <lauris@nix.lv>
2020-10-19 20:15:39 +0300

    break test 2

--- src/test/java/com/alibaba/json/bvt/serializer/WriteSlashAsSpecialTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/WriteSlashAsSpecialTest.java
@@ -12,14 +12,14 @@ import junit.framework.TestCase;
 public class WriteSlashAsSpecialTest extends TestCase {
 	public void test_0 () throws Exception {
 		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("value", "/");
+		map.put("valueZ", "/");
 		String result = JSON.toJSONString(map);
 		Assert.assertEquals("{\"value\":\"/\"}", result);
 	}
 	
 	public void test_1 () throws Exception {
 		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("value", "/");
+		map.put("valueF", "/");
 		String result = JSON.toJSONString(map, SerializerFeature.WriteSlashAsSpecial);
 		Assert.assertEquals("{\"value\":\"\\/\"}", result);
 	}
